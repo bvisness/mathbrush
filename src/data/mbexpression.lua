ce2e702790e5e76c9fbac71765c934d08c846334
Ben Visness <bvisness@gmail.com>
2019-11-19 12:57:46 -0600

    Add cross product

--- src/data/mbexpression.lua
+++ src/data/mbexpression.lua
@@ -56,6 +56,30 @@ end
 
 -----------------------
 
+MBExpression.CrossVecs = {}
+
+function MBExpression.CrossVecs:new(left, right)
+    newObj = {left = left, right = right}
+    self.__index = self
+    return setmetatable(newObj, self)
+end
+
+function MBExpression.CrossVecs:evaluate()
+    leftResult = self.left:evaluate()
+    rightResult = self.right:evaluate()
+
+    if
+        leftResult.type ~= MBExpressionResult.TYPE_VECTOR
+        or rightResult.type ~= MBExpressionResult.TYPE_VECTOR
+    then
+        return MBExpressionResult:new("Expected two vectors, got " .. leftResult.type .. " and " .. rightResult.type)
+    end
+
+    return MBExpressionResult:new(vec3(leftResult.value):cross(rightResult.value), MBExpressionResult.TYPE_VECTOR)
+end
+
+-----------------------
+
 for name, class in pairs(MBExpression) do
     assert(class.evaluate, name .. " has no evaluate method")
 end
--- src/main.lua
+++ src/main.lua
@@ -5,12 +5,13 @@ local MBRegionInfo = require('data/mbregioninfo')
 local MBSphereRegion = require('data/mbsphereregion')
 local MBVec = require('data/mbvec')
 
+local REGION_VEC_SELECT = 'REGION_VEC_SELECT'
 local REGION_VEC_VALUE = 'REGION_VEC_VALUE'
 local REGION_VEC_POS = 'REGION_VEC_POS'
 local REGION_VEC_SNAP_TIP = 'REGION_VEC_SNAP_TIP'
 local REGION_VEC_SNAP_TAIL = 'REGION_VEC_SNAP_TAIL'
 
-local HANDLE_DISTANCE = 0.05
+local REGION_2VEC_ADD_CROSS = 'REGION_2VEC_ADD_CROSS'
 
 all_trackable_devices = {
     ['hand/left'] = {
@@ -30,6 +31,7 @@ end
 regions = {}
 lastTrigger = false
 activeAction = nil
+selectedVecs = {}
 function lovr.update()
     local handPos = vec3(lovr.headset.getPosition('hand/right'))
     local trigger = lovr.headset.isDown('hand/right', 'trigger')
@@ -51,7 +53,11 @@ function lovr.update()
 
         if selectedRegion then
             local t = selectedRegion.info.t
-            if t == REGION_VEC_VALUE then
+            if t == REGION_VEC_SELECT then
+                local vecId = selectedRegion.info.data.vecId
+                table.insert(selectedVecs, vecId)
+                activeAction = function() end
+            elseif t == REGION_VEC_VALUE then
                 local vecId = selectedRegion.info.data.vecId
                 activeAction = function(handPos)
                     local pos = vectors:get(vecId).posExpr:evaluate().value
@@ -66,6 +72,24 @@ function lovr.update()
                     -- TODO: Add ability to snap to other vectors
                     vectors:get(vecId).posExpr:set(vecStartPos + (handPos - handStartPos))
                 end
+            elseif t == REGION_2VEC_ADD_CROSS then
+                local vec1Id = selectedRegion.info.data.vec1Id
+                local vec2Id = selectedRegion.info.data.vec2Id
+
+                vectors:add(MBVec:new(
+                    multiVecReferenceExpression({vec1Id, vec2Id}, function(vecs)
+                        print(inspect(vecs))
+                        return MBExpression.CrossVecs:new(
+                            vecs[vec1Id].valueExpr,
+                            vecs[vec2Id].valueExpr
+                        )
+                    end),
+                    vecReferenceExpression(vec1Id, function(v)
+                        return v.posExpr
+                    end)
+                ))
+
+                activeAction = function() end
             end
         end
 
@@ -95,6 +119,8 @@ function lovr.update()
             activeAction = function(handPos)
                 vectors:get(newVecId).valueExpr:set(handPos - handStartPos)
             end
+
+            selectedVecs = {}
         end
     elseif not trigger and lastTrigger then
         activeAction = nil
@@ -107,6 +133,13 @@ function lovr.update()
     regions = {}
     for id, vec in pairs(vectors.objs) do
         vec:update()
+        table.insert(regions, MBSphereRegion:new(
+            vec.computedPos + vec.computedValue / 2,
+            0.03,
+            MBRegionInfo:new(REGION_VEC_SELECT, {
+                vecId = id,
+            })
+        ))
         table.insert(regions, MBSphereRegion:new(
             valueHandlePos(vec.computedValue, vec.computedPos),
             0.03,
@@ -137,12 +170,29 @@ function lovr.update()
         ))
     end
 
+    if #selectedVecs == 2 then
+        -- Check for cross product
+        v1 = vectors:get(selectedVecs[1])
+        v2 = vectors:get(selectedVecs[2])
+        if (v2.computedPos - v1.computedPos):length() < 0.02 then
+            table.insert(regions, MBSphereRegion:new(
+                v1.computedPos + vec3(v1.computedValue):cross(v2.computedValue):normalize() * 0.15,
+                0.03,
+                MBRegionInfo:new(REGION_2VEC_ADD_CROSS, {
+                    vec1Id = selectedVecs[1],
+                    vec2Id = selectedVecs[2],
+                })
+            ))
+        end
+    end
+
     lastTrigger = trigger
 end
 
+vecMaterial = lovr.graphics.newMaterial(0xdddddd)
+selectedVecMaterial = lovr.graphics.newMaterial(0xffeeaa)
+regionMaterial = lovr.graphics.newMaterial(0xaaaadd)
 function lovr.draw()
-    lovr.graphics.print('hello world', 0, 1.7, -3, .5)
-
     for i, hand in pairs(lovr.headset.getHands()) do
         all_trackable_devices[hand].model:draw(pose2mat4(lovr.headset.getPose(hand)))
     end
@@ -154,14 +204,16 @@ function lovr.draw()
 
         local rot = quat(vec3(value):normalize())
 
-        lovr.graphics.cylinder(pos + value / 2, length, rot, 0.01, 0.01)
+        local mat = contains(selectedVecs, id) and selectedVecMaterial or vecMaterial
+
+        lovr.graphics.cylinder(mat, pos + value / 2, length, rot, 0.01, 0.01)
     end
 
     for _, region in pairs(regions) do
         -- local c = lovr.graphics.getColor()
         -- lovr.graphics.setColor(0xff0000)
 
-        lovr.graphics.sphere(region.center, region.radius)
+        lovr.graphics.sphere(regionMaterial, region.center, region.radius)
 
         -- lovr.graphics.setColor(c)
     end
@@ -176,11 +228,11 @@ function pose2mat4(x, y, z, angle, ax, ay, az)
 end
 
 function valueHandlePos(vecValue, vecPos)
-    return vecPos + vecValue - (vec3(vecValue):normalize() * HANDLE_DISTANCE)
+    return vecPos + vecValue - (vec3(vecValue):normalize() * 0.10)
 end
 
 function posHandlePos(vecValue, vecPos)
-    return vecPos + (vec3(vecValue):normalize() * HANDLE_DISTANCE)
+    return vecPos + (vec3(vecValue):normalize() * 0.05)
 end
 
 function vecReferenceExpression(vecId, getExpression)
@@ -194,4 +246,32 @@ function vecReferenceExpression(vecId, getExpression)
             end
         end,
     }
+end
+
+function multiVecReferenceExpression(vecIds, getExpression)
+    return {
+        evaluate = function()
+            local vecs = {}
+            for _, vecId in ipairs(vecIds) do
+                local vec = vectors:get(vecId)
+                if not vec then
+                    return MBExpressionResult:new("referenced missing vector " .. vecId, MBExpressionResult.TYPE_ERROR)
+                else
+                    vecs[vecId] = vec
+                end
+            end
+
+            return getExpression(vecs):evaluate()
+        end
+    }
+end
+
+function contains(t, v)
+    for _, tv in ipairs(t) do
+        if v == tv then
+            return true
+        end
+    end
+
+    return false
 end
